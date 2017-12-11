e466a81b0c34a3cdcc7413399166bb2d0920206f
Sander Koenders <sanderkoenders@gmail.com>
2017-12-11 19:43:40 +0100

    Test(WebpackTranspiler): Add tests for initial run of the Transpiler

--- test/unit/WebpackTranspilerSpec.ts
+++ test/unit/WebpackTranspilerSpec.ts
@@ -1,23 +1,46 @@
 import WebpackTranspiler from '../../src/WebpackTranspiler';
+import PresetLoader, * as presetLoader from '../../src/presetLoader/PresetLoader';
+import * as sinon from 'sinon';
 import { Config } from 'stryker-api/config';
 import { Position } from 'stryker-api/core';
 import { expect, assert } from 'chai';
 
 describe('WebpackTranspiler', () => {
   let webpackTranspiler: WebpackTranspiler;
-  
+  let sandbox: sinon.SinonSandbox;
+
+  // Stubs
+  let presetLoaderStub: { loadPreset: sinon.SinonStub }
+
   beforeEach(() => {
-    webpackTranspiler = new WebpackTranspiler({ config: new Config, keepSourceMaps: false })
+    sandbox = sinon.sandbox.create();
+
+    presetLoaderStub = sinon.createStubInstance(PresetLoader);
+    sandbox.stub(presetLoader, 'default').returns(presetLoaderStub);
+
+    const config: Config = new Config;
+    config.set({ project: 'Angular' });
+
+    webpackTranspiler = new WebpackTranspiler({ config, keepSourceMaps: false });
   });
 
-  it('should throw a not implemented error when calling the transpile method', async () => {
-    try {
-      await webpackTranspiler.transpile([]);
-      
-      assert(false, 'Transpile method should throw an error');
-    } catch(err) {
-      expect(err.message).to.equal('Method not implemented.');
-    }
+  afterEach(() => sandbox.restore());
+
+  it('should call the presetloader with the configured project when the transpile method is called initially', async () => {
+    await webpackTranspiler.transpile([]);
+    await webpackTranspiler.transpile([]);
+
+    assert(presetLoaderStub.loadPreset.called, 'loadPreset not called');
+    assert(presetLoaderStub.loadPreset.calledOnce, 'loadPreset called more than once');
+    assert(presetLoaderStub.loadPreset.calledWith('angular'), `loadPreset not called with 'angular'`);
+  });
+
+  it('should use \'default\' as preset when none is provided', async () => {
+    const webpackTranspiler = new WebpackTranspiler({ config: new Config, keepSourceMaps: false });
+
+    await webpackTranspiler.transpile([]);
+
+    assert(presetLoaderStub.loadPreset.calledWith('default'), `loadPreset not called with 'default'`);
   });
 
   it('should throw a not implemented error when calling the getMappedLocation method', () => {
@@ -27,9 +50,9 @@ describe('WebpackTranspiler', () => {
     };
 
     const fileLocation: { fileName: string, start: Position, end: Position } = {
-        fileName: "test",
-        start: position,
-        end: position
+      fileName: "test",
+      start: position,
+      end: position
     }
 
     expect(webpackTranspiler.getMappedLocation.bind(this, fileLocation)).to.throw(Error, 'Method not implemented.');
