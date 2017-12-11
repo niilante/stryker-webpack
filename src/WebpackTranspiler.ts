9bc5047b3894f5371b448f4b03ddc0cb761a009b
Sander Koenders <sanderkoenders@gmail.com>
2017-12-11 19:43:19 +0100

    Feat(Initial): Add initial run to transpiler class

--- src/WebpackTranspiler.ts
+++ src/WebpackTranspiler.ts
@@ -1,20 +1,43 @@
-import {TranspilerOptions, Transpiler, TranspileResult, FileLocation} from 'stryker-api/transpile';
-import {File} from 'stryker-api/core';
+import { TranspilerOptions, Transpiler, TranspileResult, FileLocation } from 'stryker-api/transpile';
+import { File } from 'stryker-api/core';
+// import { Config } from 'stryker-api/config';
+import PresetLoader from './presetLoader/PresetLoader';
+// import WebpackCompiler from './compiler/WebpackCompiler';
+// import WebpackPreset from './presetLoader/WebpackPreset';
 
 class WebpackTranspiler implements Transpiler {
-    public constructor(options: TranspilerOptions) {
+  private project: string;
+  private presetLoader: PresetLoader;
+  private initialized: boolean;
+  // private webpackCompiler: WebpackCompiler;
 
+  public constructor(options: TranspilerOptions) {
+    this.project = options.config.project || 'default';
+    this.presetLoader = new PresetLoader;
+    this.initialized = false;
+  }
+
+  public transpile(files: Array<File>): Promise<TranspileResult> {
+    if (!this.initialized) {
+      this.initialize();
+      this.initialized = true;
     }
 
-    public async transpile(files: Array<File>): Promise<TranspileResult> {
-        throw new Error('Method not implemented.');
-    }
+    return Promise.resolve({
+      error: null,
+      outputFiles: []
+    });
+  }
 
-    public getMappedLocation(sourceFileLocation: FileLocation): FileLocation {
-        // Waiting for a decision on how this is going to be implemented in the future
-        // Return a 'Method nog implemented' error for now.
-        throw new Error('Method not implemented.');
-    }
+  private initialize() {
+    this.presetLoader.loadPreset(this.project.toLowerCase());
+  }
+
+  public getMappedLocation(sourceFileLocation: FileLocation): FileLocation {
+    // Waiting for a decision on how this is going to be implemented in the future
+    // Return a 'Method nog implemented' error for now.
+    throw new Error('Method not implemented.');
+  }
 }
 
 export default WebpackTranspiler;
