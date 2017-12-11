9e27aac89d58c48173e686eb5aad28d260df6007
Sander Koenders <sanderkoenders@gmail.com>
2017-12-11 17:12:08 +0100

    Refactor(TsLint): Fix linting issues

--- src/WebpackTranspiler.ts
+++ src/WebpackTranspiler.ts
@@ -1,5 +1,5 @@
-import {TranspilerOptions, Transpiler, TranspileResult, FileLocation} from "stryker-api/transpile";
-import {File} from "stryker-api/core";
+import {TranspilerOptions, Transpiler, TranspileResult, FileLocation} from 'stryker-api/transpile';
+import {File} from 'stryker-api/core';
 
 class WebpackTranspiler implements Transpiler {
     public constructor(options: TranspilerOptions) {
@@ -7,13 +7,13 @@ class WebpackTranspiler implements Transpiler {
     }
 
     public async transpile(files: Array<File>): Promise<TranspileResult> {
-        throw new Error("Method not implemented.");
+        throw new Error('Method not implemented.');
     }
 
     public getMappedLocation(sourceFileLocation: FileLocation): FileLocation {
         // Waiting for a decision on how this is going to be implemented in the future
-        // Return a "Method nog implemented" error for now.
-        throw new Error("Method not implemented.");
+        // Return a 'Method nog implemented' error for now.
+        throw new Error('Method not implemented.');
     }
 }
 
