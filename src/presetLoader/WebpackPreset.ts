8ccae84cc9d19ae6a578c8bfd182e8b82847cd99
Sander Koenders <sanderkoenders@gmail.com>
2017-12-11 19:41:42 +0100

    Feat(WebpackPreset): Add interface to which presets must comply

---
+++ src/presetLoader/WebpackPreset.ts
@@ -0,0 +1,14 @@
+import { Configuration } from 'webpack';
+import { TextFile } from 'stryker-api/core';
+
+export default interface WebpackPreset {
+  /**
+   * Returns the actual webpack configuration file that is needed by Webpack to transpile and bundle the project
+   */
+  getWebpackConfig(): Configuration;
+
+  /**
+   * Returns all extra files that are necessary for Webpack to transpile and bundle a project
+   */
+  getInitFiles(): Array<TextFile>;
+}
