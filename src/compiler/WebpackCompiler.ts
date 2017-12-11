923745e7c0c9c2420cd4f6881f11b5f961a2be99
Sander Koenders <sanderkoenders@gmail.com>
2017-12-11 20:17:12 +0100

    Feat(WebpackCompiler): Add placeholder functions

--- src/compiler/WebpackCompiler.ts
+++ src/compiler/WebpackCompiler.ts
@@ -1,3 +1,16 @@
+import { Configuration } from "webpack";
+import { File } from 'stryker-api/src/core/File';
+
 export default class WebpackCompiler {
-  
+  constructor(webpackConfiguration: Configuration) {
+
+  }
+
+  public replace(files: Array<File>): void {
+    throw new Error('Method not implemented');
+  }
+
+  public emit(): Array<File> {
+    throw new Error('Method not implemented');
+  }
 }
