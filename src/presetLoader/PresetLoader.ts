4b21c9b0e619c09db190db87f8b8b46e961e16b4
Sander Koenders <sanderkoenders@gmail.com>
2017-12-12 15:02:37 +0100

    Feat(DefaultPreset): Add default preset to presetLoader

--- src/presetLoader/PresetLoader.ts
+++ src/presetLoader/PresetLoader.ts
@@ -1,4 +1,5 @@
 import WebpackPreset from './WebpackPreset';
+import DefaultPreset from './DefaultPreset';
 
 export default class PresetLoader {
   private loader: NodeRequire;
@@ -7,14 +8,14 @@ export default class PresetLoader {
     // Default to require when none is provided in the constructor, used in test scenarios
     this.loader = loader || /* istanbul ignore next */ require;
   }
-  
+
   public loadPreset(id: string): WebpackPreset {
-    const preset = this.requirePreset(id);
+    const preset = (id === 'default') ? new DefaultPreset() : this.requirePreset(`stryker-webpack-${id}-preset`);
 
     try {
       this.checkInterfaceFunctions(preset);
     } catch (err) {
-      throw new Error(err.message.replace('<presetId>', id));
+      throw new Error(err.message.replace('<presetId>', `stryker-webpack-${id}-preset`));
     }
 
     return preset;
@@ -22,19 +23,21 @@ export default class PresetLoader {
 
   private requirePreset(id: string): WebpackPreset {
     try {
-      return this.loader(`stryker-webpack-${id}-preset`);
+      const constructor = this.loader(id);
+
+      return new constructor
     } catch {
-      throw new Error(`Cannot find Stryker Webpack preset for '${id}', try to run npm i stryker-webpack-${id}-preset to install it`);
+      throw new Error(`Cannot find Stryker Webpack preset '${id}', try to run npm i ${id} to install it`);
     }
   }
 
   private checkInterfaceFunctions(preset: WebpackPreset): void {
-    if(!preset.getInitFiles) {
-      throw new Error(`Cannot find property 'getInitFiles' on 'stryker-webpack-<presetId>-preset'`);
+    if (!preset.getInitFiles) {
+      throw new Error(`Cannot find property 'getInitFiles' on '<presetId>'`);
     }
 
-    if(!preset.getWebpackConfig) {
-      throw new Error(`Cannot find property 'getWebpackConfig' on 'stryker-webpack-<presetId>-preset'`);
+    if (!preset.getWebpackConfig) {
+      throw new Error(`Cannot find property 'getWebpackConfig' on '<presetId>'`);
     }
   }
 }
