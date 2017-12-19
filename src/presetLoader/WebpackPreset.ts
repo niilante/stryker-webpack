17c20bb50609d13af33c472293ce32db2ef727da
Sander Koenders <sanderkoenders@gmail.com>
2017-12-19 12:53:15 +0100

    Feat(WebpackPreset): Add support for config location

--- src/presetLoader/WebpackPreset.ts
+++ src/presetLoader/WebpackPreset.ts
@@ -5,7 +5,7 @@ export default interface WebpackPreset {
   /**
    * Returns the actual webpack configuration file that is needed by Webpack to transpile and bundle the project
    */
-  getWebpackConfig(projectRoot: string): Configuration;
+  getWebpackConfig(projectRoot: string, configLocation?: string): Configuration;
 
   /**
    * Returns all extra files that are necessary for Webpack to transpile and bundle a project
